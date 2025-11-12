5d2be0234482c1485118b9eea0f118849b958ef9
kuangliang <3105242026@qq.com>
2025-11-12 11:48:16 +0800

    init

--- web/static/elements/translator.js
+++ web/static/elements/translator.js
@@ -3,6 +3,8 @@ class TranslatorElement extends HTMLElement{
         super();
         this.attachShadow({mode: "open"})
 
+        this.isTranslating = false; // 是否正在翻译
+
         fetch("./static/elements/templates/translator.html")
             .then((response) => response.text())
             .then((html) => {
@@ -10,6 +12,20 @@ class TranslatorElement extends HTMLElement{
                 const template = document.querySelector("#template_translator");
                 const content = template.content.cloneNode(true);
                 this.shadowRoot.appendChild(content);
+                
+                this.inputEdit = this.shadowRoot.querySelector("#input_edit");
+                this.outputEdit = this.shadowRoot.querySelector("#output_edit");
+
+                this.buttonTranslate = this.shadowRoot.querySelector("#botton_translate");
+                this.buttonTranslate.addEventListener("click", (e) => {
+                    // 获取输入栏的文本内容
+                    const text = this.inputEdit.textContent;
+
+                    this.outputEdit.textContent = text;
+                    
+                    // 清空输入栏的文本内容
+                    this.inputEdit.textContent = "";
+                })
             })
     }
 }

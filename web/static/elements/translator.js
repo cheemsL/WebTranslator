f6014c7f1eb14fee96c9a585397d1a97e91f7ad5
kuangliang <3105242026@qq.com>
2025-11-12 15:13:20 +0800

    init

--- web/static/elements/translator.js
+++ web/static/elements/translator.js
@@ -3,8 +3,6 @@ class TranslatorElement extends HTMLElement{
         super();
         this.attachShadow({mode: "open"})
 
-        this.isTranslating = false; // 是否正在翻译
-
         fetch("./static/elements/templates/translator.html")
             .then((response) => response.text())
             .then((html) => {
@@ -12,34 +10,31 @@ class TranslatorElement extends HTMLElement{
                 const template = document.querySelector("#template_translator");
                 const content = template.content.cloneNode(true);
                 this.shadowRoot.appendChild(content);
-                
-                this.inputEdit = this.shadowRoot.querySelector("#input_edit");
-                this.outputEdit = this.shadowRoot.querySelector("#output_edit");
 
-                this.buttonTranslate = this.shadowRoot.querySelector("#botton_translate");
-                this.buttonTranslateAllow = this.buttonTranslate.querySelector("#allow");
-                this.buttonTranslateSquare = this.buttonTranslate.querySelector("#square");
-                this.buttonTranslate.addEventListener("click", (e) => {
-                    if (this.buttonTranslateAllow.style.display !== "none"){
-                        this.buttonTranslateAllow.style.display = "none";
-                        this.buttonTranslateSquare.style.display = "block";
-                    } else {
-                        this.buttonTranslateAllow.style.display = "block";
-                        this.buttonTranslateSquare.style.display = "none";
-                    }
-
-                    // 获取输入栏的文本内容
-                    const text = this.inputEdit.textContent;
-
-                    this.outputEdit.textContent = text;
-
-                    // 清空输入栏的文本内容
-                    this.inputEdit.textContent = "";
-                })
+                this.initialize();
             })
     }
 
-    
+    initialize(){
+        this.inputEdit = this.shadowRoot.querySelector("#input_edit");
+        this.outputEdit = this.shadowRoot.querySelector("#output_edit");
+
+        this.buttonTranslate = this.shadowRoot.querySelector("#botton_translate");
+        this.buttonAudio = this.shadowRoot.querySelector("#botton_audio");
+
+        this.buttonTranslate.addEventListener("click", (e) => {
+            
+            // 获取输入栏的文本内容
+            const text = this.inputEdit.textContent;
+
+            this.outputEdit.textContent = text;
+
+            // 清空输入栏的文本内容
+            this.inputEdit.textContent = "";
+        })
+    }
+
+
 
 }
 
